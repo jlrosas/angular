a4e5e9cf7ab485e35a19bb54a26c18a9746a2450
Jose L Rosas <jrosas47@gmail.com>
2023-04-05 12:45:46 -0500

    se agrego pagina con rutas y para el 404

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
+  template: `
+  <mat-toolbar color="primary">
+    <span>My Store</span>
+</mat-toolbar>
+  `,
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent{
 
 }
