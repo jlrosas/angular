27ca005c36483ddccc7860d07a0a527b1bbeafb7
Jose L Rosas <jrosas47@gmail.com>
2023-04-07 15:07:20 -0500

    se agrego 3 observables para actualizar el carro cantidad y total, solo se muestra la cantidad en header

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProductsService } from './services/products.service';
 import { tap } from 'rxjs/operators';
 import { Product } from './interfaces/product.interface';
+import { ShoppingCartService } from 'src/app/shared/services/shopping-cart.service';
 
 @Component({
   selector: 'app-products',
@@ -11,7 +12,7 @@ import { Product } from './interfaces/product.interface';
 export class ProductsComponent implements OnInit {
   products!: Product[];
 
-  constructor(private productSvc: ProductsService){}
+  constructor(private productSvc: ProductsService, private shoppingCartSvc: ShoppingCartService){}
 
   ngOnInit(): void {
     this.productSvc.getProducts()
@@ -19,5 +20,9 @@ export class ProductsComponent implements OnInit {
       tap((products: Product[]) => this.products = products )
     )
     .subscribe();
+  }
+  addToCart(product: Product):void {
+    //console.log('add to cart', product);
+    this.shoppingCartSvc.updateCart(product);
   }
 }
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
+import { ShoppingCartService } from '../../services/shopping-cart.service';
 
 @Component({
   selector: 'app-header',
   template: `
   <mat-toolbar color="primary">
     <span>My Store</span>
+    {{quantity$ | async | json }} - Quantity
 </mat-toolbar>
   `,
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent{
+  quantity$ = this.shoppingCartSvc.quantityAction$;
+  total$ = this.shoppingCartSvc.totalActions$;
+  cart$ = this.shoppingCartSvc.cartActions$;
+  constructor(private shoppingCartSvc: ShoppingCartService){
 
+  }
 }
