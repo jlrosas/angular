48102d9aee8bbcb7f2a9dc4cb5eb697504820f07
Jose L Rosas <jrosas47@gmail.com>
2023-04-07 22:55:49 -0500

    se muestran productos en la pagina de shopping usando el subjectbehavior

---
+++ src/app/pages/checkout/details/details.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { ShoppingCartService } from 'src/app/shared/services/shopping-cart.service';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.scss']
+})
+export class DetailsComponent {
+  total$ = this.shoppingCartSvc.totalActions$;
+  cart$ = this.shoppingCartSvc.cartActions$;
+
+  constructor(private shoppingCartSvc: ShoppingCartService){
+
+  }
+}
